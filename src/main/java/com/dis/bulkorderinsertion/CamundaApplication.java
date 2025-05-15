b008a842e9ff983c5b988709b93c16832d54c731
hemanth126 <hemanthsoldier7095@gmail.com>
2025-05-15 16:34:30 +0530

    v1

--- src/main/java/com/dis/bulkorderinsertion/CamundaApplication.java
+++ src/main/java/com/dis/bulkorderinsertion/CamundaApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CamundaApplication {
 
   public static void main(String... args) {
+    System.out.print("lksrdlkgflsdkfgklsdfl");
     SpringApplication.run(CamundaApplication.class, args);
   }
 
